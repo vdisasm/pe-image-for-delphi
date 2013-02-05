4363fdf36215605291e58c3a48dfa3fac07a0151
vdisasmdev@gmail.com <vdisasmdev@gmail.com@dd2e5b98-e578-cf88-7dd0-36a083828ea8>
2013-02-05 11:55:36 +0000

    * update ResourceExtract example

--- Examples/03 Resources/ResourceExtract.dpr
+++ Examples/03 Resources/ResourceExtract.dpr
@@ -19,16 +19,25 @@ var
 function TMyImg.MyTraverse(Node: TResourceTreeNode; ud: pointer): boolean;
 var
   Leaf: TResourceTreeLeafNode;
-  FileName: string;
+  FileName, ParentName: string;
   Written: uint32;
 begin
   // Need only leaf nodes (data).
   if Node.IsLeaf then
   begin
     Leaf := Node as TResourceTreeLeafNode;
+
+    // Leaf should always have parent (branch), though it's just example.
+    if (Leaf.Parent <> nil) then
+      ParentName := (Leaf.Parent as TResourceTreeBranchNode).GetSafeName
+    else
+      ParentName := '';
+
     // Make file name.
-    FileName := Format('tmp\rsrc_%x_%x_%x', [Leaf.DataRVA, Leaf.DataSize, Leaf.Codepage]);
-    // Dump resource.
+    FileName := Format('tmp\rsrc_%s_%x_%x_%x',
+      [ParentName, Leaf.DataRVA, Leaf.DataSize, Leaf.Codepage]);
+
+    // Dump raw resource.
     Written := self.DumpRegionToFile(FileName, Leaf.DataRVA, Leaf.DataSize);
   end;
   Result := True; // continue
