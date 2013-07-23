4f1ab6ac29d69123441b9bdd83956e3f059c19b3
vdisasmdev@gmail.com <vdisasmdev@gmail.com@dd2e5b98-e578-cf88-7dd0-36a083828ea8>
2013-07-23 19:28:09 +0000

    Added PE.Search unit (to search byte patterns in sections)

---
+++ PE.Search.pas
@@ -0,0 +1,83 @@
+unit PE.Search;
+
+interface
+
+uses
+  PE.Section;
+
+{
+  *
+  * Search byte pattern in ASection starting from AOffset.
+  * Result is True if found and false otherwise.
+  * AOffset will be set to last position scanned.
+  *
+  * Each byte of AMask is AND'ed with source byte and compared to pattern.
+  * AMask can be smaller than APattern (or empty), but cannot be bigger.
+  *
+  * Example:
+  *
+  *              AA ?? BB should be represented like:
+  *   APattern:  AA 00 BB
+  *   AMask:     AA 00 BB
+  *
+  *              AA 00 BB should be represented like:
+  *   APattern:  AA 00 BB
+  *   AMask:     AA FF BB
+  *
+}
+function SearchBytes(
+  const ASection: TPESection;
+  const APattern: array of byte;
+  const AMask: array of byte;
+  var AOffset: UInt32
+  ): boolean;
+
+implementation
+
+function SearchBytes;
+var
+  pSrc: PByte;
+  Mask: byte;
+  LastOffset: UInt32;
+  i: integer;
+  MaskLeft: integer;
+begin
+  if Length(APattern) = 0 then
+    Exit(False);
+
+  if (AOffset + Length(APattern)) > ASection.AllocatedSize then
+    Exit(False);
+
+  pSrc := @ASection.Mem[AOffset];
+  LastOffset := ASection.AllocatedSize - Length(APattern);
+
+  while AOffset <= LastOffset do
+  begin
+    Result := True;
+    MaskLeft := Length(AMask);
+    for i := 0 to High(APattern) do
+    begin
+      if MaskLeft <> 0 then
+        Mask := AMask[i]
+      else
+        Mask := $FF;
+
+      if (pSrc[i] and Mask) <> APattern[i] then
+      begin
+        Result := False;
+        break;
+      end;
+
+      if MaskLeft <> 0 then
+        dec(MaskLeft);
+    end;
+
+    if Result then
+      break;
+
+    inc(AOffset);
+    inc(pSrc);
+  end;
+end;
+
+end.
