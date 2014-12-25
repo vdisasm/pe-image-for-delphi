73fdec8c60b6f62abe645d2fa1f53b762dcfcfcd
vdisasmdev <vdisasmdev@dd2e5b98-e578-cf88-7dd0-36a083828ea8>
2014-12-25 14:10:40 +0000

    * PE.Imports unit small refactoring.

--- PE.Imports.pas
+++ PE.Imports.pas
@@ -17,64 +17,37 @@ type
   private
     FLibsByName: TLibTree;
     procedure LibTreeItemNotify(Sender: TObject; const Item: TPEImportLibrary; Action: TCollectionNotification);
+    function GetCount: integer; inline;
   public
     constructor Create;
     destructor Destroy; override;
 
     procedure Clear;
 
-    function Count: integer; inline;
-
     // Add existing library.
     // Libraries are sorted by name (case insensitive).
-    procedure Add(Lib: TPEImportLibrary);
+    // Result is same library.
+    function Add(Lib: TPEImportLibrary): TPEImportLibrary; inline;
 
     // Find library by name (first occurrence). Result is nil if not found.
     function FindLib(const LibName: String): TPEImportLibrary;
+
+    // Find library by name (first occurrence). If it's not found create and
+    // add new library.
+    function FetchLib(const LibName: String): TPEImportLibrary;
 
     // Add new import function (by IAT RVA).
     procedure AddNew(RVA: TRVA; const LibName: String; Fn: TPEImportFunction); overload;
     procedure AddNew(RVA: TRVA; const LibName, FuncName: String; Ordinal: uint16 = 0); overload; inline;
 
     property LibsByName: TLibTree read FLibsByName;
+
+    property Count: integer read GetCount;
   end;
 
 implementation
 
 { TPEImports }
-
-procedure TPEImports.Add(Lib: TPEImportLibrary);
-begin
-  FLibsByName.Add(Lib);
-end;
-
-procedure TPEImports.AddNew(RVA: TRVA; const LibName: String; Fn: TPEImportFunction);
-var
-  Lib: TPEImportLibrary;
-begin
-  Lib := FindLib(LibName);
-  if Lib = nil then
-  begin
-    Lib := TPEImportLibrary.Create(LibName);
-    Add(Lib);
-  end;
-  Lib.Functions.Add(Fn);
-end;
-
-procedure TPEImports.AddNew(RVA: TRVA; const LibName, FuncName: String; Ordinal: uint16);
-begin
-  AddNew(RVA, LibName, TPEImportFunction.Create(RVA, FuncName, Ordinal));
-end;
-
-procedure TPEImports.Clear;
-begin
-  FLibsByName.Clear;
-end;
-
-function TPEImports.Count: integer;
-begin
-  Result := FLibsByName.Count;
-end;
 
 constructor TPEImports.Create;
 begin
@@ -82,7 +55,7 @@ begin
   FLibsByName := TLibTree.Create(
     function(const A, B: TPEImportLibrary): Boolean
     begin
-      Result := A.Name.ToLower < B.Name.ToLower;
+      result := A.Name.ToLower < B.Name.ToLower;
     end);
   FLibsByName.OnNotify := LibTreeItemNotify;
 end;
@@ -91,6 +64,28 @@ destructor TPEImports.Destroy;
 begin
   FLibsByName.Free;
   inherited;
+end;
+
+procedure TPEImports.LibTreeItemNotify(Sender: TObject; const Item: TPEImportLibrary; Action: TCollectionNotification);
+begin
+  if Action = cnRemoved then
+    Item.Free;
+end;
+
+function TPEImports.GetCount: integer;
+begin
+  result := FLibsByName.Count;
+end;
+
+procedure TPEImports.Clear;
+begin
+  FLibsByName.Clear;
+end;
+
+function TPEImports.Add(Lib: TPEImportLibrary): TPEImportLibrary;
+begin
+  FLibsByName.Add(Lib);
+  result := Lib;
 end;
 
 function TPEImports.FindLib(const LibName: String): TPEImportLibrary;
@@ -103,16 +98,27 @@ begin
     ptr := FLibsByName.Find(key);
     if ptr = nil then
       exit(nil);
-    Result := ptr^.K;
+    result := ptr^.K;
   finally
     key.Free;
   end;
 end;
 
-procedure TPEImports.LibTreeItemNotify(Sender: TObject; const Item: TPEImportLibrary; Action: TCollectionNotification);
+function TPEImports.FetchLib(const LibName: String): TPEImportLibrary;
 begin
-  if Action = cnRemoved then
-    Item.Free;
+  result := FindLib(LibName);
+  if not Assigned(result) then
+    result := Add(TPEImportLibrary.Create(LibName));
+end;
+
+procedure TPEImports.AddNew(RVA: TRVA; const LibName: String; Fn: TPEImportFunction);
+begin
+  FetchLib(LibName).Functions.Add(Fn);
+end;
+
+procedure TPEImports.AddNew(RVA: TRVA; const LibName, FuncName: String; Ordinal: uint16);
+begin
+  AddNew(RVA, LibName, TPEImportFunction.Create(RVA, FuncName, Ordinal));
 end;
 
 end.
